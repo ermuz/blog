3e175bcfb13f62b52c573a712f9a614779640c91
ermuz <ZhangLin_SC@163.com>
2022-11-14 00:03:32 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cd ./dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ermuz/blog.git main:gh-pages
+git push -f git@github.com:ermuz/blog.git master:gh-pages
 
 cd -
