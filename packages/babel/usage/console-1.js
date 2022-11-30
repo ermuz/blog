858b4a6b5772e61fd2a867fecffbf2c2903c51b7
ermuz <zhanglin_sc@163.com>
2022-11-30 23:04:52 +0800

    fix: lint

--- packages/babel/usage/console-1.js
+++ packages/babel/usage/console-1.js
@@ -1,6 +1,6 @@
-import * as parser from "@babel/parser";
-import _traverse from "@babel/traverse";
-import _generate from "@babel/generator";
+import * as parser from '@babel/parser';
+import _traverse from '@babel/traverse';
+import _generate from '@babel/generator';
 import types from '@babel/types';
 
 const traverse = _traverse.default;
@@ -9,19 +9,24 @@ const generate = _generate.default;
 const sourceCode = `console.log(1)`;
 
 const ast = parser.parse(sourceCode, {
-    sourceType: 'unambiguous'
+    sourceType: 'unambiguous',
 });
 
 traverse(ast, {
     CallExpression(path) {
-        if (types.isMemberExpression(path.node.callee)
-            && path.node.callee.object.name === 'console'
-            && ['log', 'warn', 'info', 'error', 'debug'].includes(path.node.callee.property.name)
+        if (
+            types.isMemberExpression(path.node.callee) &&
+            path.node.callee.object.name === 'console' &&
+            ['log', 'warn', 'info', 'error', 'debug'].includes(
+                path.node.callee.property.name,
+            )
         ) {
             const { column, line } = path.node.loc.start;
-            path.node.arguments.unshift(types.stringLiteral(`filename:(${line},${column})`));
+            path.node.arguments.unshift(
+                types.stringLiteral(`filename:(${line},${column})`),
+            );
         }
-    }
+    },
 });
 
 const { code, map } = generate(ast, { sourceMaps: true });
