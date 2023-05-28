9ab6b039ddb0d58791b49782485edca54cfbfc68
ermuz <ZhangLin_SC@163.com>
2023-05-28 23:55:15 +0800

    build: pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    extends: ['plugin:@typescript-eslint/recommended'],
-    parserOptions: {
-        parser: '@typescript-eslint/parser', // 解析 .ts 文件
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        'semi': [2, 'always']
-    }
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser', // 解析 .ts 文件
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: [2, 'always'],
+  },
 };
